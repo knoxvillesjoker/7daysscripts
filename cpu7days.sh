c407229cb114a3a1c672486aeaee70b5b045444a
knoxvillesjoker <knoxvilles_joker@yahoo.com>
2025-08-22 22:44:32 -0400

    Update cpu7days.sh

--- cpu7days.sh
+++ cpu7days.sh
@@ -3,3 +3,4 @@ top -b -n 1 -d 3 -o +%CPU | sed -e '1,/PID/d' | head -4 | ts '[%Y-%m-%d %H:%M:%S
 
 #This will output the top 4 processor memory hogs and append them to the cpu_usage log
 #You must create cpu_usage.log in /usr/local/bin.  And you must add this to crontab by invoking sudo crontab -e.  It will not run properly otherwise
+# add this line in crontab: @hourly /usr/local/bin/cpu7days.sh
