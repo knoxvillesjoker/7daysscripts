e38bcee31d3fdaec7618429cb95131e035df6917
knoxvillesjoker <knoxvilles_joker@yahoo.com>
2025-09-06 20:20:09 -0400

    Update cleanlog.sh

--- cleanlog.sh
+++ cleanlog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat /dev/null > /usr/local/bin/cpu_usage.log
+cat /dev/null > /usr/local/bin/telnet.log
 
 # This clears the cpu_usage.log
 # Run this once weekly with this line in crontab
